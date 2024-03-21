62ea86b566f9a9db5d00fba4442375a701500580
jxf1002 <jiaxianfu@yunfutech.com>
2024-03-21 16:42:02 +0800

    update logo

--- components/Header.vue
+++ components/Header.vue
@@ -2,6 +2,9 @@
   <header
     class="flex items-center bg-background/75 backdrop-blur border-b -mb-px px-[20px] sticky top-0 z-50 border-gray-200 dark:border-gray-800">
     <div class="flex">
+      <div class="flex items-center text-[32px] mr-[5px] text-primary-500 dark:text-primary-400">
+        <UIcon name="i-heroicons-home-20-solid" />
+      </div>
       <NuxtLink to="/">
         <p class="flex text-[24px] bold hover:text-primary-500 dark:hover:text-primary-400">贾师傅的小站</p>
       </NuxtLink>
