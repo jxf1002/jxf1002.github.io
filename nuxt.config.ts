3460338f3a9f7ed75918b0b296938e366e104f2d
jxf1002 <jiaxianfu@yunfutech.com>
2024-03-21 15:13:38 +0800

    增加sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxtjs/device"],
+  modules: ["@nuxt/ui", "@nuxtjs/device", "@nuxtjs/sitemap"],
+  site: {
+    url: "https://www.justful.cn",
+  },
 })
