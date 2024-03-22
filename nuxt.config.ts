6262e801a06b13a3deb72ce8c2978e9d5373d3c0
jxf1002 <jiaxianfu@yunfutech.com>
2024-03-22 10:48:01 +0800

    add google gtm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxtjs/device", "@nuxtjs/sitemap"],
+  modules: [
+    "@nuxt/ui",
+    "@nuxtjs/device",
+    "@nuxtjs/sitemap",
+    "@zadigetvoltaire/nuxt-gtm",
+  ],
   site: {
     url: "https://www.justful.cn",
   },
   sitemap: {
     autoLastmod: true,
+  },
+  gtm: {
+    id: "GTM-PBRKZL5T",
   },
 })
