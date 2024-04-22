5cf8b3eb8ee28a3a8cef50287dd147f3c9f981a0
jxf1002 <jiaxianfu@yunfutech.com>
2024-04-22 20:23:53 +0800

    解决页面跳转bug

--- composables/useHome.ts
+++ composables/useHome.ts
@@ -2,13 +2,13 @@ export default function () {
   const dataTypes = [
     {
       id: 1,
-      route: "/college",
+      route: "/college/2024/",
       title: "大学排行榜",
       description: "2023年软科、校友会、武书连大学排行数据汇总。",
     },
     {
       id: 2,
-      route: "/tourism",
+      route: "/tourism/",
       title: "5A级旅游景区",
       description: "全国5A景区汇总。",
     },
--- pages/college/[id].vue
+++ pages/college/[id].vue
@@ -8,7 +8,7 @@ useSeoMeta({
 const { isMobile } = useDevice()
 
 const route = useRoute()
-const year = parseInt(route.query.year) || 2024
+const year = parseInt(route.params.id)
 
 const { q, columns, mobileColumns, rows, page, pageCount, total, sort } = useCollege(year)
 
@@ -20,8 +20,8 @@ if (isMobile) {
 }
 
 const toYear = (year) => {
-  window.location.href = `/college/?year=${year}`
-  // window.location.reload()
+  const router = useRouter()
+  router.push({ name: 'college-id', params: { id: year } })
 }
 </script>
 
@@ -38,10 +38,13 @@ const toYear = (year) => {
       <template #header>
         <h1 class="mx-auto text-[40px] bold">大学排行榜</h1>
         <p class="mx-auto text-[14px] mt-[10px] mb-[5px] text-gray-500">（基于{{ year }}年数据制作，使用3个排行榜平均值排名）</p>
-        <UButton label="2024年" :disabled="year === 2024" @click="toYear(2024)"
-          :color="year === 2024 ? 'gray' : 'purple'" />
-        <UButton label="2023年" class="ml-[20px]" :disabled="year === 2023" @click="toYear(2023)"
-          :color="year === 2023 ? 'gray' : 'purple'" />
+        <NuxtLink to="/college/2024/">
+          <UButton label="2024年" :disabled="year === 2024" :color="year === 2024 ? 'gray' : 'purple'" />
+        </NuxtLink>
+        <NuxtLink to="/college/2023/">
+          <UButton label="2023年" class="ml-[20px]" :disabled="year === 2023"
+            :color="year === 2023 ? 'gray' : 'purple'" />
+        </NuxtLink>
       </template>
       <div class="flex px-3 py-3.5">
         <UInput v-model="q" placeholder="请输入大学名称" class="w-full md:w-auto" />
